16c7fa1268a07402ebe0e4eb29a36ea0478823b0
Danilo Ganda <daniloganda95@gmail.com>
2024-09-18 19:58:29 -0300

    feat :sparkles: create User entity test unit

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,20 @@
+import { UserEntity } from "../../user.entity";
+import {faker} from "@faker-js/faker";
+
+describe("UserEntity unit tests", ()=>{
+  it("Constructor method", ()=>{
+
+      const props = {
+        name: faker.person.fullName(),
+        email: faker.internet.email(),
+        password: faker.internet.password()
+      }
+
+      const sut = new UserEntity(props);
+
+      expect(sut).toBeDefined();
+      expect(sut.props.name).toEqual(props.name);
+      expect(sut.props.email).toEqual(props.email);
+      expect(sut.props.password).toEqual(props.password);
+  });
+});
