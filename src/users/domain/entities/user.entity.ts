701a1208d8b54aef55c2286fd380949bbb7be55c
Danilo Ganda <daniloganda95@gmail.com>
2024-09-18 19:58:12 -0300

    feat :sparkles: create User entity

--- src/users/domain/entities/user.entity.ts
+++ src/users/domain/entities/user.entity.ts
@@ -1,1 +1,12 @@
-export class UserEntity {}
+export type UserProps = {
+  name: String,
+  email: String,
+  password: String,
+  createdAt?: Date
+}
+
+export class UserEntity {
+  constructor(public readonly props: UserProps){
+    this.props.createdAt = this.props.createdAt ?? new Date();
+  }
+}
