67c665b0ae8528f12b61c92eeb5e83bde563bb5c
Danilo Ganda <daniloganda95@gmail.com>
2024-09-17 21:47:31 -0300

    feat(env) :sparkles: Implement EnvConfig

---
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { EnvConfig } from './env-config-interface';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class EnvConfigService implements EnvConfig{
+
+  constructor(private readonly configService:ConfigService){}
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'));
+  }
+
+  getNodeEnv(): string {
+    return String(this.configService.get<string>('NodeEnv'));
+  }
+}
