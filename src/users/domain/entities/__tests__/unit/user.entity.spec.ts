a55fe103ac991585de5fd025be168839479d2957
Danilo Ganda <daniloganda95@gmail.com>
2024-09-18 20:04:18 -0300

    refactor(test): simplify tests for User entity

--- src/users/domain/entities/__tests__/unit/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -1,17 +1,21 @@
-import { UserEntity } from "../../user.entity";
+import { UserEntity, UserProps } from "../../user.entity";
 import {faker} from "@faker-js/faker";
 
 describe("UserEntity unit tests", ()=>{
+  let props: UserProps;
+  let sut: UserEntity
+
+  beforeEach(()=>{
+    props = {
+      name: faker.person.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password()
+    }
+
+    sut = new UserEntity(props);
+  });
+
   it("Constructor method", ()=>{
-
-      const props = {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password()
-      }
-
-      const sut = new UserEntity(props);
-
       expect(sut).toBeDefined();
       expect(sut.props.name).toEqual(props.name);
       expect(sut.props.email).toEqual(props.email);
